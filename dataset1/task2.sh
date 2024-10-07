6be4d1e93d4ec92331311805861db6e3ff0f83cf
Anish Mulay <51531494+AnishMulay@users.noreply.github.com>
2024-10-07 22:18:50 +0000

    improved task2.sh

--- dataset1/task2.sh
+++ dataset1/task2.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-grep -l "sample" file* | xargs -I {} sh -c '
-  count=$(grep -o "CSC510" "{}" | wc -l)
-  if [ "$count" -ge 3 ]; then
-    echo "$count $(wc -c < "{}") {}"
-  fi
-' | sort -rn | uniq | sort -k1,1nr -k2,2nr | sed 's/file_/filtered_/'
+grep -l "sample" file* | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "$count $(wc -c < "{}") {}"; fi' | sort -k1,1nr -k2,2nr | sed 's/file_/filtered_/'
