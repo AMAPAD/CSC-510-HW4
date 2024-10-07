fded1fb55293bbabe45084a219958c19ee75c214
Anish Mulay <51531494+AnishMulay@users.noreply.github.com>
2024-10-07 21:36:45 +0000

    made task2 shell commands into a single pipe

--- dataset1/task2.sh
+++ dataset1/task2.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# task2.sh
-
-grep -l "sample" file* | while read file; do
-  count=$(grep -o "CSC510" "$file" | wc -l)
+grep -l "sample" file* | xargs -I {} sh -c '
+  count=$(grep -o "CSC510" "{}" | wc -l)
   if [ "$count" -ge 3 ]; then
-    echo "$count $file"
+    echo "$count $(wc -c < "{}") {}"
   fi
-done | sort -rn | uniq -c | sort -rn | while read count occurrences file; do
-  ls -l "$file" | gawk '{print $5, $NF}' | while read size filename; do
-    echo "$occurrences $size $filename" | sed 's/file_/filtered_/'
-  done
-done | sort -k1,1nr -k2,2nr
+' | sort -rn | uniq | sort -k1,1nr -k2,2nr | sed 's/file_/filtered_/'
